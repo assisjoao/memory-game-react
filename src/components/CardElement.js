a8b2f39057fdca09f3aa840019eeefe450ca2bab
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-08 07:59:28 -0300

    New feature using React

--- src/components/CardElement.js
+++ src/components/CardElement.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CardElement(props){
     return (
-        <div id={props.card.id} className='card'>
+        <div onClick={() => {props.handleFlip(props.card)}} id={props.card.id} className={`card ${props.card.flipped ? 'flip' : ''}`}>
             <div className='card_front'>
                 <img className='icon' 
                 src={`assets/images/${props.card.icon}.png`} 
